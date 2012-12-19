2c73c940d4ceb3a1cf28a280da74d6a7d7c25524
bainternet <admin@bainternet.info>
2012-12-19 14:56:03 +0200

    updated version number

--- class-usage-demo.php
+++ class-usage-demo.php
@@ -3,7 +3,7 @@
 Plugin Name: Demo Admin Page
 Plugin URI: http://en.bainternet.info
 Description: My Admin Page Class usage demo
-Version: 1.0.6
+Version: 1.0.8
 Author: Bainternet, Ohad Raz
 Author URI: http://en.bainternet.info
 */
